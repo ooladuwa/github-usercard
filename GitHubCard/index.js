55e1780ad146459d163588b350e2bb4c48dd49f8
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-01-14 12:51:23 -0500

    completed MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
+/*
+STEP 1: using axios, send a GET request to the following URL
+(replacing the placeholder with your Github name):
+https://api.github.com/users/<your name>
+*/
 // const result = axios.get("https://api.github.com/users/ooladuwa");
 // console.log(result);
-
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,12 +27,32 @@ import axios from "axios";
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
-
+    
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
-*/
+    */
 
+/*
+     STEP 3: Create a function that accepts a single object as its only argument.
+       Using DOM methods and properties, create and return the following markup:
+   
+       <div class="card">
+         <img src={image url of user} />
+         <div class="card-info">
+           <h3 class="name">{users name}</h3>
+           <p class="username">{users user name}</p>
+           <p>Location: {users location}</p>
+           <p>Profile:
+             <a href={address to users github page}>{address to users github page}</a>
+           </p>
+           <p>Followers: {users followers count}</p>
+           <p>Following: {users following count}</p>
+           <p>Bio: {users bio}</p>
+         </div>
+       </div>
+   */
 const followersArray = [
+  "ooladuwa",
   "tetondan",
   "dustinmyers",
   "justsml",
@@ -55,15 +74,16 @@ function githubCardMaker(obj) {
   const following = document.createElement("p");
   const bio = document.createElement("p");
 
-  // console.log(card);
   // setting class names,
   card.classList.add("card");
   cardInfo.classList.add("card-info");
   name.classList.add("name");
   login.classList.add("username");
+
   // attributes,
   avatar_url.setAttribute("src", obj.avatar_url);
   url.setAttribute("href", obj.url);
+
   // text
   name.textContent = obj.name;
   login.textContent = obj.login;
@@ -92,45 +112,18 @@ function githubCardMaker(obj) {
 }
 
 const cards = document.querySelector(".cards");
+console.log(cards);
+// let cardsArray = Array.from(cardsArray);
 
-// cards.forEach((card) => {
-axios
-  .get("https://api.github.com/users/ooladuwa")
-  .then((result) => {
-    const card = githubCardMaker(result.data);
-    cards.appendChild(card);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-console.log(githubCardMaker(followersArray));
-// });
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+followersArray.forEach((follower) => {
+  axios
+    .get(`https://api.github.com/users/${follower}`)
+    .then((result) => {
+      const card = githubCardMaker(result.data);
+      cards.appendChild(card);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(githubCardMaker(followersArray));
+});
